0111cc7eff1affeaa13fbb0797c22a13c126218a
Edmund Urbani <edmund.urbani@liland.com>
2022-12-26 22:28:05 +0100

    initial commit with card type properties

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// App.tsx
+import { Client } from 'boardgame.io/react';
+
+import { BertleRoyale } from './Game';
+
+const App = Client({
+  game: BertleRoyale,
+});
+export default App;
---
+++ src/Board.tsx
@@ -0,0 +1,11 @@
+// Board.tsx
+import type { BoardProps } from 'boardgame.io/react';
+import type { BertleRoyaleState } from './Game.js'
+
+interface MyGameProps extends BoardProps<BertleRoyaleState> {
+  // Additional custom properties for your component
+}
+
+export function MyGameBoard(props: MyGameProps) {
+  // Your game board
+}
